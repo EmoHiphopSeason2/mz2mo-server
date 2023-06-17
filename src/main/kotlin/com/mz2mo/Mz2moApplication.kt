f2e2d388d1ed6c5f58815d796ac646124eac6ed9
kdomo <dongho5088@naver.com>
2023-06-18 00:23:07 +0900

    init: prject init

---
+++ src/main/kotlin/com/mz2mo/Mz2moApplication.kt
@@ -0,0 +1,11 @@
+package com.mz2mo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Mz2moApplication
+
+fun main(args: Array<String>) {
+    runApplication<Mz2moApplication>(*args)
+}
---
+++ src/test/kotlin/com/mz2mo/Mz2moApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mz2mo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Mz2moApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
