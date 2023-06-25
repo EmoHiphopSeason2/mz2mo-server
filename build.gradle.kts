f843128e36a21ee453cb14b4ed27de9f776c0235
지인호 [Raul] <raul@daangn.com>
2023-06-25 21:48:38 +0900

    🔧 configurations for persistence datasource

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,12 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
