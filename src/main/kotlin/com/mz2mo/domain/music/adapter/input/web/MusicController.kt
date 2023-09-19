6ff376663a2634cb98063d7ba52d5113b1e94fd6
kdomo <dongho5088@naver.com>
2023-09-19 19:31:41 +0900

    fix: endpoint 변경

--- src/main/kotlin/com/mz2mo/domain/music/adapter/input/web/MusicController.kt
+++ src/main/kotlin/com/mz2mo/domain/music/adapter/input/web/MusicController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -27,13 +28,13 @@ class MusicController(
         return music.map { MusicQueryResponse.toResponse(it) }
     }
 
-    @PostMapping("/play/{musicId}/users/{userId}")
-    fun playMusic(@PathVariable musicId: String, @PathVariable userId: String) {
+    @PostMapping("/{musicId}/listeners")
+    fun playMusic(@PathVariable musicId: String, @RequestParam userId: String) {
         musicPlayUseCase.playMusic(musicId, userId)
     }
 
-    @GetMapping("/users/{userId}")
-    fun queryMyMusic(@PathVariable userId: String): List<MusicQueryResponse> {
+    @GetMapping("/recent")
+    fun queryRecentMusic(@RequestParam userId: String): List<MusicQueryResponse> {
         val music = musicQueryUseCase.queryRecentlyPlayedMusic(userId)
         return music.map { MusicQueryResponse.toResponse(it) }
     }
